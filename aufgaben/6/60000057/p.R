3fbe557cf121453e5b4fbe4389407790a3968fef
Andreas Müller <afm@othello.othello.ch>
2017-11-22 22:58:05 +0100

    Aufgabe 60000057

---
+++ aufgaben/6/60000057/p.R
@@ -0,0 +1,17 @@
+#
+# p.R -- display power law samples
+#
+# (c) 2017 Prof Dr Andreas Müller, Hochschule Rapperswil
+#
+
+d <- read.csv("data.csv")
+d$F = (100 - d$j) / 100
+d$logF = log(d$F)
+d$logx = log(d$x)
+regression = lm(logF ~ logx, d)
+regression
+pdf("d.pdf", 7, 5);
+plot(d$F ~ d$x, col = "red", log = "xy",  xlab = "x", ylab = "log(1-F(x))")
+abline(regression, col = "blue")
+
+
