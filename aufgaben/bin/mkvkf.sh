2e9748f0b836880876471afe3c7b8e8659afc6b6
Andreas Müller <andreas.mueller@ost.ch>
2025-06-07 09:49:22 +0200

    add new problems, VKF

---
+++ aufgaben/bin/mkvkf.sh
@@ -0,0 +1,63 @@
+#
+# mkvkf.sh -- create solutions for lecture problems
+#
+# (c) 2025 Prof Dr Andreas Müller
+#
+
+buildchapter () {
+	if [ ! -d vkf ]
+	then
+		mkdir vkf
+	fi
+	echo argument: $1
+	awk -F, 'BEGIN {
+		woche = '"${1}"'
+	}
+	/^%/ {
+		next
+	}
+	{
+		if ((woche == 0) || (woche == $1)) {
+			printf("%d %d %d %d\n", $1, $2, $3, $4)
+		}
+	}
+	END {
+	}' vkf.csv | while read woche kapitel nummer aufgabe
+	do
+		echo Woche: ${woche}
+		echo Kapitel: ${kapitel}
+		echo Nummer: ${nummer}
+		echo Aufgabe: ${aufgabe}
+		nummer1=`expr ${nummer} - 1`
+		pdf=vkf/${woche}.${kapitel}.`expr ${nummer}`.pdf
+		sed 	-e 's/KAPITEL/'"${kapitel}"'/g' \
+			-e 's/NUMMER/'"${nummer1}"'/g' \
+			-e 's/AUFGABE/'"${aufgabe}"'/g' macros/vkf.template \
+			> vkf.tex
+		pdflatex --output-directory=work vkf.tex >/dev/null 2>&1
+		if [ -r work/vkf.pdf ]
+		then
+			mv work/vkf.pdf ${pdf}
+			rm vkf.tex
+		else
+			echo failed to produce ${pdf}
+			echo see vkf/vkf.log for details
+			exit 1
+		fi
+	done
+}
+
+if [ $# -gt 0 ]
+then
+	while [ $# -gt 0 ]
+	do
+		chapter=$1
+		echo "working on chapter ${chapter}"
+		buildchapter ${chapter}
+		shift
+	done
+else
+	buildchapter 0
+fi
+
+exit 0
